fa8a3a28644abba8549442ea9efec6b03a43273a
Joshua Yee <joshuayee1001@gmail.com>
2023-05-14 18:57:58 -0700

    added authentication code for registering

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:hibye_flutterchatapp/widgets/widgets.dart';
 import 'package:hibye_flutterchatapp/pages/auth/login_page.dart';
 
+import '../../service/auth_service.dart';
+
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
@@ -11,115 +13,140 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  bool _isLoading = false;
   final formKey = GlobalKey<FormState>();
   String email = "";
   String password = "";
   String fullName = "";
+  AuthService authService = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  const SizedBox(height: 10),
-                  Image.asset("assets/HiByeLogo.jpg"),
-                  TextFormField(
-                      decoration: textInputDecoration.copyWith(
-                          labelText: "Full Name",
-                          labelStyle:
-                              TextStyle(color: Colors.black.withOpacity(0.5)),
-                          prefixIcon: Icon(Icons.person, color: Colors.black)),
-                      onChanged: (val) {
-                        setState(() {
-                          fullName = val;
-                          //print(email);
-                        });
-                      },
-                      validator: (val) {
-                        if (val!.isNotEmpty) {
-                          return null;
-                        } else {
-                          return "Name cannot be empty";
-                        }
-                      }),
-                  const SizedBox(height: 15),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                        labelText: "Email",
-                        labelStyle:
-                            TextStyle(color: Colors.black.withOpacity(0.5)),
-                        prefixIcon: Icon(Icons.email, color: Colors.black)),
-                    onChanged: (val) {
-                      setState(() {
-                        email = val;
-                        //print(email);
-                      });
-                    },
-                    validator: (val) {
-                      return RegExp(
-                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                              .hasMatch(val!)
-                          ? null
-                          : "Please enter a valid email";
-                    },
-                  ),
-                  const SizedBox(height: 15),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: textInputDecoration.copyWith(
-                        labelText: "Password",
-                        labelStyle:
-                            TextStyle(color: Colors.black.withOpacity(0.5)),
-                        prefixIcon: Icon(Icons.lock, color: Colors.black)),
-                    validator: (val) {
-                      if (val!.length < 6) {
-                        return "Password must be at least 6 characters";
-                      } else {
-                        return null;
-                      }
-                    },
-                    onChanged: (val) {
-                      setState(() {
-                        password = val;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                      onPressed: () {
-                        register();
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey),
-                      child: Text("Register")),
-                  SizedBox(height: 30),
-                  Text.rich(TextSpan(
-                      text: "Already have an account? ",
-                      style: const TextStyle(color: Colors.black, fontSize: 14),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: "Sign in here",
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator(color: Colors.grey))
+          : SingleChildScrollView(
+              child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        const SizedBox(height: 10),
+                        Image.asset("assets/HiByeLogo.jpg"),
+                        TextFormField(
+                            decoration: textInputDecoration.copyWith(
+                                labelText: "Full Name",
+                                labelStyle: TextStyle(
+                                    color: Colors.black.withOpacity(0.5)),
+                                prefixIcon:
+                                    Icon(Icons.person, color: Colors.black)),
+                            onChanged: (val) {
+                              setState(() {
+                                fullName = val;
+                                //print(email);
+                              });
+                            },
+                            validator: (val) {
+                              if (val!.isNotEmpty) {
+                                return null;
+                              } else {
+                                return "Name cannot be empty";
+                              }
+                            }),
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Email",
+                              labelStyle: TextStyle(
+                                  color: Colors.black.withOpacity(0.5)),
+                              prefixIcon:
+                                  Icon(Icons.email, color: Colors.black)),
+                          onChanged: (val) {
+                            setState(() {
+                              email = val;
+                              //print(email);
+                            });
+                          },
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val!)
+                                ? null
+                                : "Please enter a valid email";
+                          },
+                        ),
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Password",
+                              labelStyle: TextStyle(
+                                  color: Colors.black.withOpacity(0.5)),
+                              prefixIcon:
+                                  Icon(Icons.lock, color: Colors.black)),
+                          validator: (val) {
+                            if (val!.length < 6) {
+                              return "Password must be at least 6 characters";
+                            } else {
+                              return null;
+                            }
+                          },
+                          onChanged: (val) {
+                            setState(() {
+                              password = val;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                            onPressed: () {
+                              register();
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey),
+                            child: Text("Register")),
+                        SizedBox(height: 30),
+                        Text.rich(TextSpan(
+                            text: "Already have an account? ",
                             style: const TextStyle(
-                                color: Colors.black,
-                                decoration: TextDecoration.underline),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                nextScreen(context, const LoginPage());
-                              }),
-                      ])),
-                ],
-              ),
-            )),
-      ),
+                                color: Colors.black, fontSize: 14),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: "Sign in here",
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      decoration: TextDecoration.underline),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      nextScreen(context, const LoginPage());
+                                    }),
+                            ])),
+                      ],
+                    ),
+                  )),
+            ),
     );
   }
 
-  register() {}
+  register() async {
+    if (formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      await authService
+          .registerUserWithEmailandPassword(fullName, email, password)
+          .then((value) {
+        if (value == true) {
+        } else {
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      });
+    }
+  }
 }
---
+++ lib/service/auth_service.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hibye_flutterchatapp/service/database_service.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  Future registerUserWithEmailandPassword(
+      String fullName, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        await DatabaseService(uid: user.uid).updateUserData(fullName, email);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+}
