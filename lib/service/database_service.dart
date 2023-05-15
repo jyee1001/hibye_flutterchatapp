ecc83ef35092b72afc15edbbb8236e818fb6c514
Joshua Yee <joshuayee1001@gmail.com>
2023-05-15 16:05:21 -0700

    added login authentication

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -12,7 +12,7 @@ class DatabaseService {
       FirebaseFirestore.instance.collection("groups");
 
   //for updating the userdata
-  Future updateUserData(String fullName, String email) async {
+  Future savingUserData(String fullName, String email) async {
     return await userCollection.doc(uid).set({
       "fullName": fullName,
       "email": email,
@@ -20,5 +20,11 @@ class DatabaseService {
       "profilePic": "",
       "uid": uid,
     });
+  }
+
+  Future gettingUserData(String email) async {
+    QuerySnapshot snapshot =
+        await userCollection.where("email", isEqualTo: email).get();
+    return snapshot;
   }
 }
