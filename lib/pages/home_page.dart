8a270eaed78ec9686e7e516fbd1b382c170e35d5
Joshua Yee <joshuayee1001@gmail.com>
2023-05-15 23:30:54 -0700

    added displays on profile page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -122,9 +122,9 @@ class _HomePageState extends State<HomePage> {
               nextScreenReplace(
                   context,
                   ProfilePage(
-                      //userName: userName,
-                      //email: email,
-                      ));
+                    userName: userName,
+                    email: email,
+                  ));
             },
             contentPadding:
                 const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
