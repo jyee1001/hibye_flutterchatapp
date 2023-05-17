a798200479f15ef9ccf49f63a39f0b83ff67b418
Joshua Yee <joshuayee1001@gmail.com>
2023-05-16 17:14:49 -0700

    changed ui of message tiles

--- lib/widgets/message_tile.dart
+++ lib/widgets/message_tile.dart
@@ -31,22 +31,21 @@ class _MessageTileState extends State<MessageTile> {
             ? const EdgeInsets.only(left: 30)
             : const EdgeInsets.only(right: 30),
         padding:
-            const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+            const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 10),
         decoration: BoxDecoration(
             borderRadius: widget.sentByMe
                 ? const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(20),
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
                   )
                 : const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                    bottomRight: Radius.circular(20),
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
                   ),
-            color: widget.sentByMe
-                ? Theme.of(context).primaryColor
-                : Colors.grey[700]),
+            color:
+                widget.sentByMe ? Colors.black : Color.fromRGBO(51, 51, 51, 1)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
